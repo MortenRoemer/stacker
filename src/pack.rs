19501ff41dd7bd6bc33c68c3a802236d9cfb1f56
MortenRoemer <morten.roemer@gmx.de>
2022-02-22 20:05:29 +0100

    finalized pack trait

--- src/pack.rs
+++ src/pack.rs
@@ -10,6 +10,17 @@ pub trait Pack {
     }
 }
 
+impl Pack for bool {
+    fn pack_into(&self, writer: &mut impl io::Write) -> io::Result<usize> {
+        let value = match self {
+            true => 0x00,
+            false => 0xFF,
+        };
+        let buffer = [value];
+        writer.write(&buffer)
+    }
+}
+
 impl Pack for u8 {
     fn pack_into(&self, writer: &mut impl io::Write) -> io::Result<usize> {
         let buffer = [*self];
@@ -19,56 +30,70 @@ impl Pack for u8 {
 
 impl Pack for u16 {
     fn pack_into(&self, writer: &mut impl io::Write) -> io::Result<usize> {
-        let buffer = self.to_le_bytes();
+        let buffer = self.to_be_bytes();
         writer.write(&buffer)
     }
 }
 
 impl Pack for u32 {
     fn pack_into(&self, writer: &mut impl io::Write) -> io::Result<usize> {
-        let buffer = self.to_le_bytes();
+        let buffer = self.to_be_bytes();
         writer.write(&buffer)
     }
 }
 
 impl Pack for u64 {
     fn pack_into(&self, writer: &mut impl io::Write) -> io::Result<usize> {
-        let buffer = self.to_le_bytes();
+        let buffer = self.to_be_bytes();
         writer.write(&buffer)
     }
 }
 
 impl Pack for u128 {
     fn pack_into(&self, writer: &mut impl io::Write) -> io::Result<usize> {
-        let buffer = self.to_le_bytes();
+        let buffer = self.to_be_bytes();
         writer.write(&buffer)
     }
 }
 
 impl Pack for i16 {
     fn pack_into(&self, writer: &mut impl io::Write) -> io::Result<usize> {
-        let buffer = self.to_le_bytes();
+        let buffer = self.to_be_bytes();
         writer.write(&buffer)
     }
 }
 
 impl Pack for i32 {
     fn pack_into(&self, writer: &mut impl io::Write) -> io::Result<usize> {
-        let buffer = self.to_le_bytes();
+        let buffer = self.to_be_bytes();
         writer.write(&buffer)
     }
 }
 
 impl Pack for i64 {
     fn pack_into(&self, writer: &mut impl io::Write) -> io::Result<usize> {
-        let buffer = self.to_le_bytes();
+        let buffer = self.to_be_bytes();
         writer.write(&buffer)
     }
 }
 
 impl Pack for i128 {
     fn pack_into(&self, writer: &mut impl io::Write) -> io::Result<usize> {
-        let buffer = self.to_le_bytes();
+        let buffer = self.to_be_bytes();
+        writer.write(&buffer)
+    }
+}
+
+impl Pack for f32 {
+    fn pack_into(&self, writer: &mut impl io::Write) -> io::Result<usize> {
+        let buffer = self.to_be_bytes();
+        writer.write(&buffer)
+    }
+}
+
+impl Pack for f64 {
+    fn pack_into(&self, writer: &mut impl io::Write) -> io::Result<usize> {
+        let buffer = self.to_be_bytes();
         writer.write(&buffer)
     }
 }
@@ -92,5 +117,108 @@ impl<T: Pack> Pack for [T] {
         }
 
         Ok(written)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn pack_bool() {
+        let value = false;
+        let bytes = value.pack_to_vec().unwrap();
+        assert_eq!(bytes, [0xFF]);
+    }
+
+    #[test]
+    fn pack_u8() {
+        let value: u8 = 2;
+        let bytes = value.pack_to_vec().unwrap();
+        assert_eq!(bytes, [0x02]);
+    }
+
+    #[test]
+    fn pack_u16() {
+        let value: u16 = 2;
+        let bytes = value.pack_to_vec().unwrap();
+        assert_eq!(bytes, [0x00, 0x02]);
+    }
+
+    #[test]
+    fn pack_u32() {
+        let value: u32 = 2;
+        let bytes = value.pack_to_vec().unwrap();
+        assert_eq!(bytes, [0x00, 0x00, 0x00, 0x02]);
+    }
+
+    #[test]
+    fn pack_u64() {
+        let value: u64 = 2;
+        let bytes = value.pack_to_vec().unwrap();
+        assert_eq!(bytes, [0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02]);
+    }
+
+    #[test]
+    fn pack_u128() {
+        let value: u128 = 2;
+        let bytes = value.pack_to_vec().unwrap();
+        assert_eq!(bytes, [0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02]);
+    }
+
+    #[test]
+    fn pack_i16() {
+        let value: i16 = -1;
+        let bytes = value.pack_to_vec().unwrap();
+        assert_eq!(bytes, [0xFF, 0xFF]);
+    }
+
+    #[test]
+    fn pack_i32() {
+        let value: i32 = -1;
+        let bytes = value.pack_to_vec().unwrap();
+        assert_eq!(bytes, [0xFF, 0xFF, 0xFF, 0xFF]);
+    }
+
+    #[test]
+    fn pack_i64() {
+        let value: i64 = -1;
+        let bytes = value.pack_to_vec().unwrap();
+        assert_eq!(bytes, [0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF]);
+    }
+
+    #[test]
+    fn pack_i128() {
+        let value: i128 = -1;
+        let bytes = value.pack_to_vec().unwrap();
+        assert_eq!(bytes, [0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF]);
+    }
+
+    #[test]
+    fn pack_f32() {
+        let value: f32 = -1.0;
+        let bytes = value.pack_to_vec().unwrap();
+        assert_eq!(bytes, [0xBF, 0x80, 0x00, 0x00]);
+    }
+
+    #[test]
+    fn pack_f64() {
+        let value: f64 = -1.0;
+        let bytes = value.pack_to_vec().unwrap();
+        assert_eq!(bytes, [0xBF, 0xF0, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]);
+    }
+
+    #[test]
+    fn pack_str() {
+        let value = "abc";
+        let bytes = value.pack_to_vec().unwrap();
+        assert_eq!(bytes, [0x00, 0x00, 0x00, 0x03, 0x61, 0x62, 0x63]);
+    }
+
+    #[test]
+    fn pack_array() {
+        let value: [u8; 3] = [1, 2, 3];
+        let bytes = value.pack_to_vec().unwrap();
+        assert_eq!(bytes, [0x00, 0x00, 0x00, 0x03, 0x01, 0x02, 0x03]);
     }
 }
