814be03ec6c9fdf28c3c9edf5f6423ac26ab1417
MortenRoemer <morten.roemer@gmx.de>
2022-03-13 11:05:47 +0100

    Added nonzero integers to unpack implementations

--- src/unpack.rs
+++ src/unpack.rs
@@ -1,21 +1,21 @@
 use std::error;
 use std::fmt::{self, Display, Formatter};
 use std::io;
+use std::num::*;
 use std::rc::Rc;
 use std::string::FromUtf8Error;
 use std::sync::Arc;
 
 /// Describes the ability to deserialize a struct from a sequential bytesource
-/// 
+///
 /// Any type implementing this trait has to be Sized and Owned but this contraints
 /// may change in the future
-/// 
+///
 /// It is not possible to derive this trait, because deserialization may be
 /// sensitive to order and endianness. (Big endianness is assumed for all primitives)
 pub trait Unpack {
-
     /// Tries to deserialize this struct from a given sequence of bytes
-    /// 
+    ///
     /// Deserialization may fail for one of these reasons:
     /// - any IO-Error ocurred (ErrorKind::Interrupted is ignored)
     /// - a string contained invalid UTF8 code
@@ -26,7 +26,7 @@ pub trait Unpack {
 }
 
 /// Error that may occur during deserialization
-/// 
+///
 /// There are three possible reasons deserialization may fail:
 /// - any IO-Error ocurred (ErrorKind::Interrupted is ignored)
 /// - a string contained invalid UTF8 contained
@@ -57,9 +57,7 @@ pub type Result<T> = std::result::Result<T, Error>;
 impl Unpack for bool {
     fn unpack_from(reader: &mut impl io::Read) -> Result<Self> {
         let mut bytes = [0x00];
-        reader
-            .read_exact(&mut bytes)
-            .map_err(Error::IO)?;
+        reader.read_exact(&mut bytes).map_err(Error::IO)?;
         Ok(bytes[0] != 0xFF)
     }
 }
@@ -67,99 +65,151 @@ impl Unpack for bool {
 impl Unpack for u8 {
     fn unpack_from(reader: &mut impl io::Read) -> Result<Self> {
         let mut bytes = [0x00];
-        reader
-            .read_exact(&mut bytes)
-            .map_err(Error::IO)?;
+        reader.read_exact(&mut bytes).map_err(Error::IO)?;
         Ok(bytes[0])
+    }
+}
+
+impl Unpack for NonZeroU8 {
+    fn unpack_from(reader: &mut impl io::Read) -> Result<Self> {
+        let mut bytes = [0x00];
+        reader.read_exact(&mut bytes).map_err(Error::IO)?;
+        Ok(NonZeroU8::new(bytes[0]).unwrap())
     }
 }
 
 impl Unpack for u16 {
     fn unpack_from(reader: &mut impl io::Read) -> Result<Self> {
         let mut bytes = [0x00; 2];
-        reader
-            .read_exact(&mut bytes)
-            .map_err(Error::IO)?;
+        reader.read_exact(&mut bytes).map_err(Error::IO)?;
         Ok(u16::from_be_bytes(bytes))
+    }
+}
+
+impl Unpack for NonZeroU16 {
+    fn unpack_from(reader: &mut impl io::Read) -> Result<Self> {
+        let mut bytes = [0x00; 2];
+        reader.read_exact(&mut bytes).map_err(Error::IO)?;
+        Ok(NonZeroU16::new(u16::from_be_bytes(bytes)).unwrap())
     }
 }
 
 impl Unpack for u32 {
     fn unpack_from(reader: &mut impl io::Read) -> Result<Self> {
         let mut bytes = [0x00; 4];
-        reader
-            .read_exact(&mut bytes)
-            .map_err(Error::IO)?;
+        reader.read_exact(&mut bytes).map_err(Error::IO)?;
         Ok(u32::from_be_bytes(bytes))
+    }
+}
+
+impl Unpack for NonZeroU32 {
+    fn unpack_from(reader: &mut impl io::Read) -> Result<Self> {
+        let mut bytes = [0x00; 4];
+        reader.read_exact(&mut bytes).map_err(Error::IO)?;
+        Ok(NonZeroU32::new(u32::from_be_bytes(bytes)).unwrap())
     }
 }
 
 impl Unpack for u64 {
     fn unpack_from(reader: &mut impl io::Read) -> Result<Self> {
         let mut bytes = [0x00; 8];
-        reader
-            .read_exact(&mut bytes)
-            .map_err(Error::IO)?;
+        reader.read_exact(&mut bytes).map_err(Error::IO)?;
         Ok(u64::from_be_bytes(bytes))
+    }
+}
+
+impl Unpack for NonZeroU64 {
+    fn unpack_from(reader: &mut impl io::Read) -> Result<Self> {
+        let mut bytes = [0x00; 8];
+        reader.read_exact(&mut bytes).map_err(Error::IO)?;
+        Ok(NonZeroU64::new(u64::from_be_bytes(bytes)).unwrap())
     }
 }
 
 impl Unpack for u128 {
     fn unpack_from(reader: &mut impl io::Read) -> Result<Self> {
         let mut bytes = [0x00; 16];
-        reader
-            .read_exact(&mut bytes)
-            .map_err(Error::IO)?;
+        reader.read_exact(&mut bytes).map_err(Error::IO)?;
         Ok(u128::from_be_bytes(bytes))
+    }
+}
+
+impl Unpack for NonZeroU128 {
+    fn unpack_from(reader: &mut impl io::Read) -> Result<Self> {
+        let mut bytes = [0x00; 16];
+        reader.read_exact(&mut bytes).map_err(Error::IO)?;
+        Ok(NonZeroU128::new(u128::from_be_bytes(bytes)).unwrap())
     }
 }
 
 impl Unpack for i16 {
     fn unpack_from(reader: &mut impl io::Read) -> Result<Self> {
         let mut bytes = [0x00; 2];
-        reader
-            .read_exact(&mut bytes)
-            .map_err(Error::IO)?;
+        reader.read_exact(&mut bytes).map_err(Error::IO)?;
         Ok(i16::from_be_bytes(bytes))
+    }
+}
+
+impl Unpack for NonZeroI16 {
+    fn unpack_from(reader: &mut impl io::Read) -> Result<Self> {
+        let mut bytes = [0x00; 2];
+        reader.read_exact(&mut bytes).map_err(Error::IO)?;
+        Ok(NonZeroI16::new(i16::from_be_bytes(bytes)).unwrap())
     }
 }
 
 impl Unpack for i32 {
     fn unpack_from(reader: &mut impl io::Read) -> Result<Self> {
         let mut bytes = [0x00; 4];
-        reader
-            .read_exact(&mut bytes)
-            .map_err(Error::IO)?;
+        reader.read_exact(&mut bytes).map_err(Error::IO)?;
         Ok(i32::from_be_bytes(bytes))
+    }
+}
+
+impl Unpack for NonZeroI32 {
+    fn unpack_from(reader: &mut impl io::Read) -> Result<Self> {
+        let mut bytes = [0x00; 4];
+        reader.read_exact(&mut bytes).map_err(Error::IO)?;
+        Ok(NonZeroI32::new(i32::from_be_bytes(bytes)).unwrap())
     }
 }
 
 impl Unpack for i64 {
     fn unpack_from(reader: &mut impl io::Read) -> Result<Self> {
         let mut bytes = [0x00; 8];
-        reader
-            .read_exact(&mut bytes)
-            .map_err(Error::IO)?;
+        reader.read_exact(&mut bytes).map_err(Error::IO)?;
         Ok(i64::from_be_bytes(bytes))
+    }
+}
+
+impl Unpack for NonZeroI64 {
+    fn unpack_from(reader: &mut impl io::Read) -> Result<Self> {
+        let mut bytes = [0x00; 8];
+        reader.read_exact(&mut bytes).map_err(Error::IO)?;
+        Ok(NonZeroI64::new(i64::from_be_bytes(bytes)).unwrap())
     }
 }
 
 impl Unpack for i128 {
     fn unpack_from(reader: &mut impl io::Read) -> Result<Self> {
         let mut bytes = [0x00; 16];
-        reader
-            .read_exact(&mut bytes)
-            .map_err(Error::IO)?;
+        reader.read_exact(&mut bytes).map_err(Error::IO)?;
         Ok(i128::from_be_bytes(bytes))
+    }
+}
+
+impl Unpack for NonZeroI128 {
+    fn unpack_from(reader: &mut impl io::Read) -> Result<Self> {
+        let mut bytes = [0x00; 16];
+        reader.read_exact(&mut bytes).map_err(Error::IO)?;
+        Ok(NonZeroI128::new(i128::from_be_bytes(bytes)).unwrap())
     }
 }
 
 impl Unpack for f32 {
     fn unpack_from(reader: &mut impl io::Read) -> Result<Self> {
         let mut bytes = [0x00; 4];
-        reader
-            .read_exact(&mut bytes)
-            .map_err(Error::IO)?;
+        reader.read_exact(&mut bytes).map_err(Error::IO)?;
         Ok(f32::from_be_bytes(bytes))
     }
 }
@@ -167,9 +217,7 @@ impl Unpack for f32 {
 impl Unpack for f64 {
     fn unpack_from(reader: &mut impl io::Read) -> Result<Self> {
         let mut bytes = [0x00; 8];
-        reader
-            .read_exact(&mut bytes)
-            .map_err(Error::IO)?;
+        reader.read_exact(&mut bytes).map_err(Error::IO)?;
         Ok(f64::from_be_bytes(bytes))
     }
 }
@@ -181,9 +229,7 @@ impl Unpack for String {
         let mut buffer = [0x00; 512];
 
         while len > 0 {
-            let _read = reader
-                .read(&mut buffer)
-                .map_err(Error::IO)?;
+            let _read = reader.read(&mut buffer).map_err(Error::IO)?;
 
             len = if len > buffer.len() {
                 bytes.extend_from_slice(&buffer);
@@ -248,10 +294,24 @@ mod tests {
     }
 
     #[test]
+    fn unpack_non_zero_u8() {
+        let bytes = [0xFF];
+        let value = NonZeroU8::unpack_from(&mut bytes.as_ref()).unwrap();
+        assert_eq!(value, NonZeroU8::new(255).unwrap());
+    }
+
+    #[test]
     fn unpack_u16() {
         let bytes = [0x00, 0x02];
         let value = u16::unpack_from(&mut bytes.as_ref()).unwrap();
         assert_eq!(value, 2);
+    }
+
+    #[test]
+    fn unpack_non_zero_u16() {
+        let bytes = [0x00, 0x02];
+        let value = NonZeroU16::unpack_from(&mut bytes.as_ref()).unwrap();
+        assert_eq!(value, NonZeroU16::new(2).unwrap());
     }
 
     #[test]
@@ -262,10 +322,24 @@ mod tests {
     }
 
     #[test]
+    fn unpack_non_zero_u32() {
+        let bytes = [0x00, 0x00, 0x00, 0x02];
+        let value = NonZeroU32::unpack_from(&mut bytes.as_ref()).unwrap();
+        assert_eq!(value, NonZeroU32::new(2).unwrap());
+    }
+
+    #[test]
     fn unpack_u64() {
         let bytes = [0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02];
         let value = u64::unpack_from(&mut bytes.as_ref()).unwrap();
         assert_eq!(value, 2);
+    }
+
+    #[test]
+    fn unpack_non_zero_u64() {
+        let bytes = [0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02];
+        let value = NonZeroU64::unpack_from(&mut bytes.as_ref()).unwrap();
+        assert_eq!(value, NonZeroU64::new(2).unwrap());
     }
 
     #[test]
@@ -279,10 +353,27 @@ mod tests {
     }
 
     #[test]
+    fn unpack_non_zero_u128() {
+        let bytes = [
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0x00, 0x02,
+        ];
+        let value = NonZeroU128::unpack_from(&mut bytes.as_ref()).unwrap();
+        assert_eq!(value, NonZeroU128::new(2).unwrap());
+    }
+
+    #[test]
     fn unpack_i16() {
         let bytes = [0xFF, 0xFF];
         let value = i16::unpack_from(&mut bytes.as_ref()).unwrap();
         assert_eq!(value, -1);
+    }
+
+    #[test]
+    fn unpack_non_zero_i16() {
+        let bytes = [0xFF, 0xFF];
+        let value = NonZeroI16::unpack_from(&mut bytes.as_ref()).unwrap();
+        assert_eq!(value, NonZeroI16::new(-1).unwrap());
     }
 
     #[test]
@@ -293,10 +384,24 @@ mod tests {
     }
 
     #[test]
+    fn unpack_non_zero_i32() {
+        let bytes = [0xFF, 0xFF, 0xFF, 0xFF];
+        let value = NonZeroI32::unpack_from(&mut bytes.as_ref()).unwrap();
+        assert_eq!(value, NonZeroI32::new(-1).unwrap());
+    }
+
+    #[test]
     fn unpack_i64() {
         let bytes = [0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF];
         let value = i64::unpack_from(&mut bytes.as_ref()).unwrap();
         assert_eq!(value, -1);
+    }
+
+    #[test]
+    fn unpack_non_zero_i64() {
+        let bytes = [0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF];
+        let value = NonZeroI64::unpack_from(&mut bytes.as_ref()).unwrap();
+        assert_eq!(value, NonZeroI64::new(-1).unwrap());
     }
 
     #[test]
@@ -307,6 +412,16 @@ mod tests {
         ];
         let value = i128::unpack_from(&mut bytes.as_ref()).unwrap();
         assert_eq!(value, -1);
+    }
+
+    #[test]
+    fn unpack_non_zero_i128() {
+        let bytes = [
+            0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
+            0xFF, 0xFF,
+        ];
+        let value = NonZeroI128::unpack_from(&mut bytes.as_ref()).unwrap();
+        assert_eq!(value, NonZeroI128::new(-1).unwrap());
     }
 
     #[test]
